90724f6060a17610b650aa697f8c06b26dd74ac4
Aleksandr Pasechnik <apasechnik@mediamath.com>
2017-09-22 08:49:28 -0400

    gopathonly defines foo and bar

---
+++ foo.go
@@ -0,0 +1,13 @@
+// Package gopathonly exists only in the gopath. It is not vendored into the
+// main package. Here are some docs for it.
+package gopathonly
+
+// Foo returns the string "foo"
+func Foo() string {
+	return "foo"
+}
+
+// Bar returns the string "bar"
+func Bar() string {
+	return "bar"
+}
